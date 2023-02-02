d49ab5a61a99d757e6c13ea0441ce210c01e78e2
mike <mamack1@dmacc.edu>
2023-02-01 18:33:19 -0600

    coupon_calculations assignment

--- coupon_calculations.py
+++ coupon_calculations.py
@@ -29,16 +29,16 @@ if cash_coupon == "y":
     elif cash_off == 10:
         cash_off_subtotal = price - 10
 else:
-    print("No Cash Off Coupon Added.")
+    cash_off_subtotal = price
 
 type(price)
 
 # ask for percent coupons
-percent_coupon = input("Do you have a percent off coupon?Enter y or n: ")
+percent_coupon = input("Do you have a percent off coupon? Enter y or n: ")
 
 # percent coupon if statement
 if percent_coupon == "y":
-    percent_off = float(int(input("Please enter which Cash Off coupon you have: ")))
+    percent_off = float(int(input("Please enter which Percent Off coupon you have: ")))
     if percent_off == 10:
         percent_off_subtotal = float(cash_off_subtotal - (cash_off_subtotal*.1))
     elif percent_off == 15:
@@ -46,7 +46,7 @@ if percent_coupon == "y":
     elif percent_off == 20:
         percent_off_subtotal = float(cash_off_subtotal - (cash_off_subtotal*.2))
 else:
-    print("No Percent Off Coupon Added.")
+    percent_off_subtotal = cash_off_subtotal
 
 # Add shipping
 if percent_off_subtotal <= 9.99:
@@ -66,5 +66,12 @@ total = total_after_tax + shipping_cost
 
 # Print Total
 print(f"Thanks for your purchase, your total after discounts, tax, and shipping is: ${total: 5.2f}")
+
+# test cases
+# price     cash_off        percent_off     total expected      total
+# 36.92     10              20              30.78               30.78
+# 3.67      5               0               5.95                5.95
+# 125.64    10              15              104.19              104.19
+# 65.92     0               10              62.89               62.89
 
 
